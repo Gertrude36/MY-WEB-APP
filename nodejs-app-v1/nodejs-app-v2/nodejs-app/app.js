88515a848c3ac7cc2403ecde72b8b99485b2af3c
Gertrude <irimasogertrude@gmail.com>
2025-11-28 16:28:43 +0200

    Prepare project for first release

---
+++ nodejs-app-v1/nodejs-app-v2/nodejs-app/app.js
@@ -0,0 +1,13 @@
+"scripts": {
+  "start": "node app.js"
+}
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, Kubernetes! Helm Deployment\n');
+});
+
+server.listen(3000, () => console.log('Server running on port 3000'));
+
